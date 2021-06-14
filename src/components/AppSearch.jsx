3acded0ae1f93af9d41e77ed305cc9923b800261
renzpulvira <calypsogaming098two@gmail.com>
2021-06-14 23:14:54 +0800

    Implementing leaflet geolocation search

--- src/components/AppSearch.jsx
+++ src/components/AppSearch.jsx
@@ -1,11 +1,29 @@
 import React, { useState } from "react";
 import { FiSearch } from "react-icons/fi";
+import { OpenStreetMapProvider, GeoSearchControl } from "leaflet-geosearch";
+import { map, useLeaflet } from "react-leaflet";
 
 export default function AppSearch() {
   const [searchInput, setSearchInput] = useState("");
 
   const captureSearch = (e) => {
     setSearchInput(e);
+  };
+
+  const Search = (props) => {
+    const { map } = useLeaflet(); // access to leaflet map
+    const { provider } = props;
+
+    useEffect(() => {
+      const searchControl = new GeoSearchControl({
+        provider,
+      });
+
+      map.addControl(searchControl); // this is how you add a control in vanilla leaflet
+      return () => map.removeControl(searchControl);
+    }, [props]);
+
+    return null; // don't want anything to show up from this comp
   };
 
   return (
@@ -20,6 +38,10 @@ export default function AppSearch() {
             </p>
           )}
           <input type="text" onChange={(e) => captureSearch(e.target.value)} />
+
+          <Map>
+            <Search provider={new OpenStreetMapProvider()} />
+          </Map>
         </form>
       </div>
     </div>
