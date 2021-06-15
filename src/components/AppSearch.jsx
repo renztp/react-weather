d8164389129c2f477aaf8348a1a47b9c7ebd51f8
renzpulvira <calypsogaming098two@gmail.com>
2021-06-15 20:57:16 +0800

    Implement google places and resolved errors

--- src/components/AppSearch.jsx
+++ src/components/AppSearch.jsx
@@ -1,48 +1,71 @@
 import React, { useState } from "react";
 import { FiSearch } from "react-icons/fi";
-import { OpenStreetMapProvider, GeoSearchControl } from "leaflet-geosearch";
-import { map, useLeaflet } from "react-leaflet";
+import PlacesAutocomplete, {
+  geocodeByAddress,
+  getLatLng,
+} from "react-places-autocomplete";
 
 export default function AppSearch() {
-  const [searchInput, setSearchInput] = useState("");
+  // const [searchInput, setSearchInput] = useState("");
 
-  const captureSearch = (e) => {
-    setSearchInput(e);
-  };
+  // const captureSearch = (e) => {
+  //   setSearchInput(e);
+  // };
 
-  const Search = (props) => {
-    const { map } = useLeaflet(); // access to leaflet map
-    const { provider } = props;
+  const [address, setAddress] = useState("");
 
-    useEffect(() => {
-      const searchControl = new GeoSearchControl({
-        provider,
-      });
-
-      map.addControl(searchControl); // this is how you add a control in vanilla leaflet
-      return () => map.removeControl(searchControl);
-    }, [props]);
-
-    return null; // don't want anything to show up from this comp
-  };
+  const handleSelect = async (value) => {};
 
   return (
     <div className="app-search">
       <div className="AppSidebar__nav-controls">
-        <form action="" className="app-search__form">
-          {searchInput.length > 0 ? (
+        <div className="app-search__form">
+          <PlacesAutocomplete
+            value={address}
+            onChange={setAddress}
+            onSelect={handleSelect}
+          >
+            {({
+              getInputProps,
+              suggestions,
+              getSuggestionItemProps,
+              loading,
+            }) => (
+              <div>
+                <input
+                  {...getInputProps({
+                    placeholder: "Search for a location...",
+                  })}
+                />
+                <div>{loading ? <div>Loading...</div> : null}</div>
+                {suggestions.map((suggestion, index) => {
+                  const style = {
+                    backgroundColor: suggestion.active ? "#333" : "#f2f4f6",
+                    color: suggestion.active ? "#fff" : "#333",
+                  };
+
+                  return (
+                    <div
+                      key={index}
+                      {...getSuggestionItemProps(suggestion, { style })}
+                    >
+                      {suggestion.description}
+                    </div>
+                  );
+                })}
+              </div>
+            )}
+          </PlacesAutocomplete>
+
+          {/* {searchInput.length > 0 ? (
             ""
           ) : (
             <p className="app-search__placeholder">
               <FiSearch color="#888" /> <span>Search...</span>
             </p>
           )}
-          <input type="text" onChange={(e) => captureSearch(e.target.value)} />
-
-          <Map>
-            <Search provider={new OpenStreetMapProvider()} />
-          </Map>
-        </form>
+          <input type="text" onChange={(e) => captureSearch(e.target.value)} /> */}
+        </div>
       </div>
     </div>
   );
